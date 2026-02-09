4746124c8a5a89560d28ae3fbc666188d627491d
bluemario8 <bluemario812@gmail.com>
2026-02-09 08:32:30 -0500

    Changed email size from 100 to 255 (max size of an email.) I also updated the Users class in extensions.py

--- extensions.py
+++ extensions.py
@@ -61,7 +61,7 @@ class Users(db.Model):
     username = db.Column(db.String(30), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     dob = db.Column(db.Date, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
