46d7d2460d013da9fb5b1f6f001bc46e3e1d4fe6
bluemario8 <bluemario812@gmail.com>
2026-02-13 11:25:01 -0500

    Created register form backend and the user can now create an account. Updated requirements.txt

--- blueprints/register/register.py
+++ blueprints/register/register.py
@@ -1,29 +1,71 @@
+import time
 from flask import Blueprint, render_template, request, url_for, redirect
 from flask_login import login_user
 from extensions import Users, Roles, bcrypt, db
+from flask_wtf import FlaskForm
+from wtforms import (StringField, PasswordField, DateField, SubmitField, RadioField)
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegisterForm(FlaskForm):    
+    username = StringField('Username',
+        validators=[
+            DataRequired(),
+            Length(min=2, max=30),
+        ])
+    email = StringField('Email',
+        validators=[
+            DataRequired(), 
+            Email(),
+            
+        ])
+    password = PasswordField('Password',
+        validators=[
+            DataRequired(),
+            Length(min=8, max=255),
+        ])
+    confirm_password = PasswordField('Confirm Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password'),
+        ])
+    first_name = StringField('First Name',
+        validators=[
+            DataRequired(), 
+        ])
+    last_name = StringField('Last Name',
+        validators=[
+            DataRequired(), 
+        ])
+    dob = DateField('Date Of Birth',
+        validators=[
+            DataRequired(),
+        ])
+    submit = SubmitField('Sign Up')
+
 register_bp = Blueprint("register", __name__, static_folder="static",
                   template_folder="templates")
 
 
 @register_bp.route("/register", methods=["GET", "POST"])
 def register():
+    register_form = RegisterForm()
     error = request.args.get("error", None)
-    if request.method == "POST":
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        confPassword = request.form.get('conf-password')
-        firstName = request.form.get('first_name')
-        lastName = request.form.get('last_name')
-        type = request.form.get('type')
+    if register_form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(register_form.password.data).decode('utf-8')
+        user = Users(username=register_form.username.data,
+                     email=register_form.email.data,
+                     password=hashed_password,
+                     last_name=register_form.last_name.data,
+                     first_name=register_form.first_name.data,
+                     dob=register_form.dob.data,
+        )
+        db.session.add(user)
+        db.session.commit()
+        
+        return "Success"
 
-
-
-        if not email or not password:
-            return render_template("register.html", error="Invalid input")
-
-    # roles = db.session.execute(text('SELECT * FROM roles ORDER BY role_id DESC'))
     roles = Roles.query.order_by(Roles.role_id.desc())
 
     return render_template("register.html", error=error,
-                           roles=roles)
+                           roles=roles, register_form=register_form
+    )
