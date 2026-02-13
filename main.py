7cd887b84fe245027c19a9f7f7128a4c4dfae378
Ellie Mott <elizabethmott1326@gmail.com>
2026-02-13 09:36:30 -0500

    created basic home page

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from extensions import *
 from blueprints.viewDatabase.viewDatabase import view_database_bp
 from blueprints.login.login import login_bp
 from blueprints.register.register import register_bp
+from blueprints.home.home import home_bp
 
 # register blueprints
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(view_database_bp)
+app.register_blueprint(home_bp)
 
 # quick route to see all routes
 @app.route("/routes")
