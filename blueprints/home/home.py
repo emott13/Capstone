c71869678cdf39ec1b4f9d1bef45383f5173e2b4
Ellie Mott <elizabethmott1326@gmail.com>
2026-02-13 10:41:37 -0500

    added product cards to home page

--- blueprints/home/home.py
+++ blueprints/home/home.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, request, url_for, redirect
 from flask_login import login_user
-from extensions import Users, bcrypt
+from extensions import conn
+from sqlalchemy import text
 home_bp = Blueprint("home", __name__, static_folder="static_home",
                   template_folder="templates_home")
 
 @home_bp.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    # get products categories to display on home page
+    productsCategories = conn.execute(text("SELECT category_name FROM product_categories;")).fetchall()
+    return render_template("home.html", productsCategories=productsCategories)
