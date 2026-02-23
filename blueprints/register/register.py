5a55369ca456a97ba1ef246068b15624d7f39e02
bluemario8 <bluemario812@gmail.com>
2026-02-23 10:44:05 -0500

    Login page success now shows (if account was just registered). Register password now has regex to require a lowercase, uppercase, number, and special char. Moved all register post actions into a register_post() function. This makes returning errors much simpler

--- blueprints/register/register.py
+++ blueprints/register/register.py
@@ -5,41 +5,42 @@ from sqlalchemy import text
 from extensions import Users, UserRoles, Roles, bcrypt, db
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, DateField, SubmitField, RadioField)
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, Regexp
 
 class RegisterForm(FlaskForm):    
     username = StringField('Username',
         validators=[
-            DataRequired(),
+            InputRequired(),
             Length(min=2, max=30),
         ])
     email = StringField('Email',
         validators=[
-            DataRequired(), 
+            InputRequired(), 
             Email(),
             
         ])
     password = PasswordField('Password',
         validators=[
-            DataRequired(),
+            InputRequired(),
             Length(min=8, max=255),
+            Regexp(regex="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*])", message="Password must contain at least 1 capital, 1 lowercase, 1 number, and a special character"),
         ])
     confirm_password = PasswordField('Confirm Password',
         validators=[
-            DataRequired(),
+            InputRequired(),
             EqualTo('password'),
         ])
     first_name = StringField('First Name',
         validators=[
-            DataRequired(), 
+            InputRequired(), 
         ])
     last_name = StringField('Last Name',
         validators=[
-            DataRequired(), 
+            InputRequired(), 
         ])
     dob = DateField('Date Of Birth',
         validators=[
-            DataRequired(),
+            InputRequired(),
         ])
     submit = SubmitField('Sign Up')
 
@@ -52,30 +53,8 @@ def register():
     register_form = RegisterForm()
     error = request.args.get("error", None)
     if register_form.validate_on_submit():
-        print(request.form.getlist('role_id'))
-        role_ids = request.form.getlist('role_id')
-
-        if role_ids == []:
-            error = "Need to select an account type"
-        else:
-            hashed_password = bcrypt.generate_password_hash(register_form.password.data).decode('utf-8')
-            user = Users(username=register_form.username.data,
-                        email=register_form.email.data,
-                        password=hashed_password,
-                        last_name=register_form.last_name.data,
-                        first_name=register_form.first_name.data,
-                        dob=register_form.dob.data,
-            )
-
-            db.session.add(user)
-            db.session.commit()
-
-            roles = []
-            for role_id in role_ids:
-                roles.append(UserRoles(user_id=user.user_id, role_id=role_id))
-            db.session.add_all(roles)
-            db.session.commit()
-            
+        error = register_post(register_form)
+        if not error:
             return redirect(url_for('login.login', success="Account created! Login here"))
 
     roles = Roles.query.where(text('role_name != \'admin\'')).order_by(Roles.role_id.desc())
@@ -83,3 +62,32 @@ def register():
     return render_template("register.html", error=error,
                            roles=roles, register_form=register_form,
     )
+
+
+def register_post(register_form) -> str:
+    """Runs the register post verifications and logic. Returns the error if there is any"""
+    print(request.form.getlist('role_id'))
+    role_ids = request.form.getlist('role_id')
+
+    if role_ids == []:
+        return "Need to select an account type"
+        
+    hashed_password = bcrypt.generate_password_hash(register_form.password.data).decode('utf-8')
+    user = Users(username=register_form.username.data,
+                email=register_form.email.data,
+                password=hashed_password,
+                last_name=register_form.last_name.data,
+                first_name=register_form.first_name.data,
+                dob=register_form.dob.data,
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    roles = []
+    for role_id in role_ids:
+        roles.append(UserRoles(user_id=user.user_id, role_id=role_id))
+    db.session.add_all(roles)
+    db.session.commit()
+    
+    return ""
