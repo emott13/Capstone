49365ee23379459ad7d3d2afbf51e5c167b84574
bluemario8 <bluemario812@gmail.com>
2026-02-23 09:55:38 -0500

    Added error checking for login page

--- blueprints/login/login.py
+++ blueprints/login/login.py
@@ -3,7 +3,7 @@ from flask_login import login_user
 from extensions import Users, bcrypt
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, DateField, SubmitField, RadioField)
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import Length, Email, EqualTo, InputRequired, Regexp
 
 login_bp = Blueprint("login", __name__, static_folder="static_login",
                   template_folder="templates")
@@ -11,12 +11,12 @@ login_bp = Blueprint("login", __name__, static_folder="static_login",
 class LoginForm(FlaskForm):    
     username = StringField('Username Or Email',
         validators=[
-            DataRequired(),
+            InputRequired(message="A username or email is required"),
             Length(max=255),
         ])
     password = PasswordField('Password',
         validators=[
-            DataRequired(),
+            InputRequired(),
             Length(min=8, max=255),
         ])
     submit = SubmitField('Login')
@@ -27,27 +27,31 @@ def login():
     login_form = LoginForm()
     error = request.args.get("error", None)
     success = request.args.get("success", None)
-    if request.method == "POST":
-        email = request.form.get("username")
-        password = request.form.get("password")
 
-        if not email or not password:
-            return render_template("login.html", error="Invalid input")
-
-        user = Users.query.filter_by(email=email).first()
-        user_username = Users.query.filter_by(username=email).first()
-
-        # first checks email, then checks username
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for("home.home"))
-        elif user_username and bcrypt.check_password_hash(user_username.password, password):
-            login_user(user_username)
-            return redirect(url_for("home.home"))
-        
-        else:
-            return render_template("login.html", error="Invalid username or password",
-                                   login_form=login_form)
+    # This only happens on POST request
+    if login_form.validate_on_submit():
+        error = login_post(login_form)
 
     return render_template("login.html", error=error, success=success,
                            login_form=login_form,)
+
+
+def login_post(login_form) -> str:
+    """Runs the login post verifications. Returns the error if there is any"""
+    email = request.form.get("username")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return "Invalid input"
+    user = Users.query.filter_by(email=email).first()
+    user_username = Users.query.filter_by(username=email).first()
+
+    # first checks email, then checks username
+    if user and bcrypt.check_password_hash(user.password, password):
+        login_user(user)
+        return redirect(url_for("home.home"))
+    elif user_username and bcrypt.check_password_hash(user_username.password, password):
+        login_user(user_username)
+        return redirect(url_for("home.home"))
+    else:
+        return "Invalid username or password"
