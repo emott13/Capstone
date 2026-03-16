73a687989bfeed4164fd01f855539d05fabb7cee
Ailish <snickrrdoodles@gmail.com>
2026-03-16 07:51:26 -0400

    Fixed database viewing page issue

--- extensions.py
+++ extensions.py
@@ -39,7 +39,7 @@ bcrypt = Bcrypt(app)
 # Initialize database and login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "blueprints.login.login"
+login_manager.login_view = "login.login"
 
 # Initialize DB the way we did the other times
 engine = create_engine(conn_str, echo=True)                                             
